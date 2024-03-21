61be5d29b412fbc49838dc6378a416edc99cf846
Honys <zhonys@proton.me>
2024-03-21 03:20:13 -0300

    Fix OperatorDeclaration in  @operator (should be OperatorKeyword) Add CreateConversions and CreateConversionBodies

--- DTOGenerator/TheGenerator.cs
+++ DTOGenerator/TheGenerator.cs
@@ -53,18 +53,18 @@ internal sealed class TheGenerator : IIncrementalGenerator {
         builder.Append("namespace SynoLib;\n");
         builder.Append(FetchAllNonGeneratorUsings(compilation.SyntaxTrees));
         builder.AppendLine();
-        /*foreach (var model in list) {
+        foreach (var cls in list) {
 
             SyntaxList<MemberDeclarationSyntax> members = new();
-            foreach (var member in model.Members) {
+            foreach (var member in cls.Members) {
                 if (member is FieldDeclarationSyntax or PropertyDeclarationSyntax)
                     members = members.Add(member);
             }
 
-            var newDTO = CreateDTOClass(compilation, model);
+            var newDTO = CreateDTOClass(compilation, cls);
 
             builder.Append(newDTO);
-        }*/
+        }
 
 
         context.AddSource("SynoLib.g.cs", builder.ToString());
@@ -92,6 +92,7 @@ internal sealed class TheGenerator : IIncrementalGenerator {
 
 
     private static ClassDeclarationSyntax CreateDTOClass(Compilation compilation, ClassDeclarationSyntax cls) {
+        DTOModelData data = DTOModelData.GetDTOModelData(cls);
         var tokenList = new SyntaxTokenList(SyntaxFactory.Token(whiteSpace, SyntaxKind.PublicKeyword, new()),
                                             SyntaxFactory.Token(whiteSpace, SyntaxKind.SealedKeyword, whiteSpace));
 
@@ -104,35 +105,92 @@ internal sealed class TheGenerator : IIncrementalGenerator {
             if (member is FieldDeclarationSyntax or PropertyDeclarationSyntax)
                 members = members.Add(member);
         }
+        members = members.AddRange(CreateConversions(data));
 
         return SyntaxFactory.ClassDeclaration(new(), tokenList, identifier, null, null, null, new(), members);
     }
 
-    private static void CreateConversions(DTOModelData data) {
-        SyntaxList<MemberDeclarationSyntax> members = new();
+    #region Conversions
+
+    private static SeparatedSyntaxList<MemberDeclarationSyntax> CreateConversions(DTOModelData data) {
+        SeparatedSyntaxList<MemberDeclarationSyntax> members = new();
+
+        TypeSyntax DTOTypeSyntax = SyntaxFactory.ParseTypeName(data.DTOName);
+        SyntaxToken DTOParameterToken = SyntaxFactory.Identifier("dto");
+        ParameterListSyntax DTOParameterSyntax = SyntaxFactory.ParameterList().AddParameters(
+            SyntaxFactory.Parameter(
+                new(), new SyntaxTokenList(),
+                DTOTypeSyntax.WithTrailingTrivia(whiteSpace),
+                DTOParameterToken,
+                null));
+
+        TypeSyntax ModelTypeSyntax = SyntaxFactory.ParseTypeName($"{data.ModelName}");
+        SyntaxToken ModelParameterToken = SyntaxFactory.Identifier("model");
+        ParameterListSyntax ModelParameterSyntax = SyntaxFactory.ParameterList().AddParameters(
+            SyntaxFactory.Parameter(
+                new(), new SyntaxTokenList(),
+                ModelTypeSyntax.WithTrailingTrivia(whiteSpace),
+                ModelParameterToken,
+                null));
+
+
         if (data.conversion.HasFlag(ConversionForm.Explicit)) {
+            var ModeltoDTO = SyntaxFactory.ConversionOperatorDeclaration(
+                new(),
+                publicStatic,
+                SyntaxFactory.Token(new(), SyntaxKind.ExplicitKeyword, whiteSpace),
+                DTOTypeSyntax.WithLeadingTrivia(whiteSpace),
+                ModelParameterSyntax,
+                CreateConversionBodies(data),
+                null);
+            var DTOtoModel = SyntaxFactory.ConversionOperatorDeclaration(
+                new(),
+                publicStatic,
+                SyntaxFactory.Token(new(), SyntaxKind.ExplicitKeyword, whiteSpace),
+                ModelTypeSyntax.WithLeadingTrivia(whiteSpace),
+                DTOParameterSyntax,
+                null,
+                null);
+            members = members.AddRange([DTOtoModel, ModeltoDTO]);
         }
+
+        return members;
     }
 
-    private static DTOModelData GetDTOModelData(ClassDeclarationSyntax cls) {
-        var data = new DTOModelData() {
-            ModelName = cls.Identifier.ValueText,
-            conversion = ConversionForm.Explicit
-        };
-        return data;
+    private static BlockSyntax CreateConversionBodies(DTOModelData data) {
+        SeparatedSyntaxList<ExpressionSyntax> assignments = new();
+        foreach (var property in data.PropertiesNames) {
+            var modelProperty = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, 
+                SyntaxFactory.IdentifierName("model"), SyntaxFactory.Token(SyntaxKind.DotToken), SyntaxFactory.IdentifierName(property));
+            assignments = assignments.Add(
+                SyntaxFactory.AssignmentExpression(
+                    SyntaxKind.SimpleAssignmentExpression, 
+                    SyntaxFactory.IdentifierName(property), SyntaxFactory.Token(SyntaxKind.EqualsToken), modelProperty));
+
+        }
+        var initializer = SyntaxFactory.InitializerExpression(SyntaxKind.ObjectInitializerExpression, expressions: assignments);
+        var dtoCreation = SyntaxFactory.ImplicitObjectCreationExpression(
+            SyntaxFactory.Token(SyntaxKind.NewKeyword).WithLeadingTrivia(whiteSpace),
+            SyntaxFactory.ArgumentList(),
+            initializer);
+        var block = SyntaxFactory.Block(SyntaxFactory.ReturnStatement(dtoCreation));
+        return block;
     }
 
-    public static explicit operator string(TheGenerator gen) {
-        return "";
-    }
+    #endregion
+
+    #region Random functions
+
+
+    #endregion
 
     #region Static Data
-    
+
     private static SyntaxTriviaList whiteSpace = new(SyntaxFactory.Whitespace(" "));
     private static SyntaxTriviaList endLine = new(SyntaxFactory.EndOfLine("\n"));
 
     private static SyntaxTokenList publicStatic = new(NewToken(new(), SyntaxKind.PublicKeyword, whiteSpace), NewToken(new(), SyntaxKind.StaticKeyword, whiteSpace));
-    private static SyntaxTokenList @operator = new(NewToken(new(), SyntaxKind.OperatorDeclaration, whiteSpace));
+    private static SyntaxTokenList @operator = new(NewToken(new(), SyntaxKind.OperatorKeyword, whiteSpace));
 
     #endregion
 
@@ -146,6 +204,17 @@ internal sealed class TheGenerator : IIncrementalGenerator {
 
 internal record struct DTOModelData {
     public string ModelName { get; set; }
+    public string DTOName => $"DTO<{ModelName}>";
+
+    public List<string> PropertiesNames { get; set; }
 
     public ConversionForm conversion;
+    
+    public static DTOModelData GetDTOModelData(ClassDeclarationSyntax cls) {
+        var data = new DTOModelData() {
+            ModelName = cls.Identifier.ValueText,
+            conversion = ConversionForm.Explicit
+        };
+        return data;
+    }
 }
